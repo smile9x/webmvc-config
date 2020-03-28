4b47f2ec2f4fab681c405bc302a5a6c6973cb130
Cuong Ngo <j.nmcuong@gmail.com>
2020-03-28 16:04:41 +0700

    spring mvc AppInitializer.java

---
+++ src/main/java/my/nmcuong/webmvc/MainController.java
@@ -0,0 +1,16 @@
+package my.nmcuong.webmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	
+	@ResponseBody
+	@RequestMapping("/")
+	public String message() {
+		return "Hello world";
+	}
+}
---
+++ src/main/java/my/nmcuong/webmvc/WebConfig.java
@@ -0,0 +1,10 @@
+package my.nmcuong.webmvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("my.nmcuong.webmvc")
+public class WebConfig {
+
+}
